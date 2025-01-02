8a0c9c5d6c760f5726dcea59a67d0d5977ae7f4a
TamjidBhai <tamtop2000@gmail.com>
2025-01-02 22:31:44 +0600

    added hero section

--- components/Searchbar.tsx
+++ components/Searchbar.tsx
@@ -1,17 +1,62 @@
 "use client";
-import React from "react";
+import React, { FormEvent, useState } from "react";
 
+const isValidAmazonProductURL = (url: string) => {
+  try {
+    const parsedURL = new URL(url);
+    const hostname = parsedURL.hostname;
+
+    if (
+      hostname.includes("amazon.com") ||
+      hostname.includes("amazon.") ||
+      hostname.includes("amazon")
+    ) {
+      return true;
+    }
+  } catch (error) {
+    console.log("Invalid URL: ", error);
+  }
+
+  return false;
+};
 const Searchbar = () => {
-  const handleSubmit = () => {};
+  const [searchPrompt, setSearchPrompt] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const isValidURL = isValidAmazonProductURL(searchPrompt);
+
+    if (!isValidURL) {
+      return alert("Invalid URL! Please enter a valid Amazon product URL.");
+    }
+
+    try {
+      setIsLoading(true);
+
+      //Scrape the product
+    } catch (error) {
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
   return (
     <form className="flex flex-wrap gap-4 mt-12" onSubmit={handleSubmit}>
       <input
         type="text"
+        value={searchPrompt}
+        onChange={(e) => setSearchPrompt(e.target.value)}
         placeholder="Enter Product Link"
         className="searchbar-input"
       />
-      <button type="submit" className="searchbar-btn">
-        Search
+      <button
+        type="submit"
+        className="searchbar-btn"
+        disabled={searchPrompt === ""}
+      >
+        {isLoading ? "Searching..." : "Search"}
       </button>
     </form>
   );
